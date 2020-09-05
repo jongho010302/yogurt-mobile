4eb5b2376cd3529f10e6ef278d931a4a975bf850
Honoour <hearful@icloud.com>
2020-09-05 20:05:18 +0900

    apply redux devtools plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { middleware as reduxPackMiddleware } from 'redux-pack';
-import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(reduxPackMiddleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxPackMiddleware)),
+);
 
-export default function () {
+export default function Index() {
   return (
     <Provider store={store}>
       <App />
